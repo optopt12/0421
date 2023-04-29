f50282eff8a8728db2ed64a2af3d05753b8f63b7
buddy53044 <buddy53044@gmail.com>
2023-04-29 17:50:54 +0800

    0429

--- app/src/main/java/com/example/chatbot/Fragment/ThirdFragment.kt
+++ app/src/main/java/com/example/chatbot/Fragment/ThirdFragment.kt
@@ -1,6 +1,9 @@
 package com.example.chatbot.Fragment
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Base64
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,16 +21,20 @@ import com.example.chatbot.databinding.MapShopBinding
 import com.example.chatbot.placesDetails.PlacesDetails
 import com.example.chatbot.placesDetails.data
 import com.example.chatbot.placesSearch.PlacesSearch
+import com.google.android.gms.common.internal.ImagesContract.URL
+import com.google.gson.internal.bind.TypeAdapters.URL
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.ByteArrayOutputStream
+import java.net.URL
 
 
 private var _binding: MapShopBinding? = null
 private val binding get() = _binding!!
 private lateinit var RAdapter: RestaurantListAdapter
 private var msglist: MutableList<data> = ArrayList()//建立可改變的list
-private var photorefArray: MutableList<String>  = ArrayList()
+private var photorefArray: MutableList<String> = ArrayList()
 private lateinit var placeid: String
 private var placeidArray: MutableList<String> = ArrayList()
 private lateinit var image: String
@@ -92,6 +99,7 @@ class ThirdFragment : Fragment() {
             }
         }
     }
+
     private fun SearchShop() {
         val search = binding.editText.text.toString()
         binding.button.setOnClickListener()
@@ -109,32 +117,34 @@ class ThirdFragment : Fragment() {
                     response: Response<PlacesSearch>
                 ) {
                     response.body()?.let { res ->
-                            res.results.forEach { result ->
-                                placeid = result.place_id
-                                name = result.name
-                                placeidArray.add(placeid)
-                                result.photos.forEach { photo ->
-                                    photorefArray.add(photo.photo_reference)
-                                }
+                        res.results.forEach { result ->
+                            placeid = result.place_id
+                            name = result.name
+                            placeidArray.add(placeid)
+                            result.photos.forEach { photo ->
+                                photorefArray.add(photo.photo_reference)
                             }
+                        }
                     }
-                    for(i in 0 .. photorefArray.size - 1)
-                    {
+                    for (i in 0..photorefArray.size - 1) {
                         photoref = photorefArray[i]
-                        image ="https://maps.googleapis.com/maps/api/place/photo" +
+                        image = "https://maps.googleapis.com/maps/api/place/photo" +
                                 "?maxwidth=300" +
                                 "&maxheight=200" +
                                 "&photo_reference=" + photoref +
                                 "&key=" + BuildConfig.GOOGLE_API_KEY
-                        nestedDataList.add(NestedData(image))
+                        var newimage = testuniformimage(image)
+//                        nestedDataList.add(NestedData(image))
+                        nestedDataList.add(NestedData(newimage))
+
                     }
-                    for(i in 0 .. placeidArray.size - 1)
-                    {
+                    for (i in 0..placeidArray.size - 1) {
                         placeid = placeidArray[i]
                         image = nestedDataList[i].imageUrl
                         DetailSearch(placeid, image)
                     }
                 }
+
                 override fun onFailure(
                     call: Call<PlacesSearch>,
                     t: Throwable
@@ -145,7 +155,8 @@ class ThirdFragment : Fragment() {
             })
         }
     }
-    private fun DetailSearch(placeid :String,image:String){
+
+    private fun DetailSearch(placeid: String, image: String) {
         Apiclient.googlePlaces.getPlaceDetails(
             placeID = placeid,
             language = "zh-TW",
@@ -155,13 +166,14 @@ class ThirdFragment : Fragment() {
                 call: Call<PlacesDetails>,
                 response: Response<PlacesDetails>
             ) {
-                    response.body()?.let { res ->
-                            address= res.result.formatted_address ?: ""
-                            name = res.result.name ?: ""
-                            phonenumber = res.result.formatted_phone_number ?: ""
-                    }
+                response.body()?.let { res ->
+                    address = res.result.formatted_address ?: ""
+                    name = res.result.name ?: ""
+                    phonenumber = res.result.formatted_phone_number ?: ""
+                }
                 rv(image)
             }
+
             override fun onFailure(
                 call: Call<PlacesDetails>,
                 t: Throwable
@@ -171,18 +183,42 @@ class ThirdFragment : Fragment() {
             }
         })
     }
+
     private fun rv(image: String) {
-        msglist.add(data(
-            formatted_address = address,
-            name = name,
-            formatted_phone_number = phonenumber,
-            image = image
-            ))
+        msglist.add(
+            data(
+                formatted_address = address,
+                name = name,
+                formatted_phone_number = phonenumber,
+                image = image
+            )
+        )
 //        Log.d("msg", "msglist: $msglist\n")
 //        Log.d("nestedDataList", "nestedDataList: $nestedDataList\n")
 //        Log.d("photorefArray", "photorefArray: $photorefArray\n")
 //        Log.d("image", "image: $image\n")
         RAdapter.notifyDataSetChanged()
+
+
+    }
+
+    private fun testuniformimage(image: String): String {
+        val width = 300 // 设置目标宽度
+        val height = 200 // 设置目标高度
+        val resizedImages = mutableListOf<Bitmap>()
+
+        val input = URL(image).openStream() // 从 URL 中读取图片数据流
+        val bitmap = BitmapFactory.decodeStream(input) // 解码图片数据流为 Bitmap 对象
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false) // 缩放 Bitmap 对象
+
+        resizedImages.add(resizedBitmap) // 将缩放后的 Bitmap 对象添加到列表中
+
+        val stream = ByteArrayOutputStream()
+        resizedBitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        val byteArray = stream.toByteArray()
+
+        val encodedString = Base64.encodeToString(byteArray, Base64.DEFAULT)
+        return encodedString
 
 
     }
